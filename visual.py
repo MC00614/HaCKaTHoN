dc8d3f9ba3b46e5d5cf22392b7ab26e902d0e882
MC00614 <12190588@inha.edu>
2023-02-18 22:13:35 +0900

    Update visual.py

--- visual.py
+++ visual.py
@@ -44,12 +44,12 @@ plt.plot([+10, +10], [-outline, -crosssize], 'k', linewidth = lw)
 ax = plt.axis([-outline,outline,-outline,outline])
 
 car_1 = utils.Vehicle(1, [0.1, 0], [-100, -2.5])
-car_2 = utils.Vehicle(2, [0.1, 0], [-100, -7.5])
+car_2 = utils.Vehicle(2, [0, 0.1], [2.5, -100])
 
 car_no_1_dot, = plt.plot([], [], 'ro')
 car_no_2_dot, = plt.plot([], [], 'bo')
 
-def animate(i):
+def animate(_):
     car_1.time_passed_dt()
     car_2.time_passed_dt()
     car_no_1_dot.set_data(car_1.whereiscar())
@@ -58,7 +58,7 @@ def animate(i):
     return car_no_1_dot, car_no_2_dot
 
 # create animation using the animate() function
-myAnimation = animation.FuncAnimation(fig, animate, frames=np.arange(-100.0, 100, 0.1), \
+myAnimation = animation.FuncAnimation(fig, animate, frames=np.arange(10), \
                                       interval=1, blit=True, repeat=True)
 
 plt.show()
